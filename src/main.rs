de5699a170e3de05c1110ba1da7489d2b97ae91c
phantomgoose <alex.txt@gmail.com>
2024-01-01 20:32:36 -0800

    add game of life | add border for cells

--- src/main.rs
+++ src/main.rs
@@ -3,94 +3,199 @@ use macroquad::prelude::*;
 #[derive(Clone, Copy, PartialOrd, PartialEq, Debug)]
 enum CellState {
     Alive,
-    Dying,
+    // Dying,
     Dead,
+}
+
+impl CellState {
+    fn color(&self) -> Color {
+        match self {
+            CellState::Alive => GREEN,
+            // CellState::Dying => ORANGE,
+            CellState::Dead => BLACK,
+        }
+    }
+}
+
+const ROWS: usize = 128;
+const COLUMNS: usize = 128;
+
+type SimState = [[CellState; COLUMNS]; ROWS];
+
+/// Fetches the number of cells of interest surrounding the current cell in the matrix
+fn get_cell_count<F>(state: SimState, r: usize, c: usize, predicate: F) -> u8
+where
+    F: Fn(CellState) -> bool,
+{
+    let mut count = 0;
+
+    // up
+    if r > 0 && predicate(state[r - 1][c]) {
+        count += 1;
+    }
+
+    // up left
+    if r > 0 && c > 0 && predicate(state[r - 1][c - 1]) {
+        count += 1;
+    }
+
+    // up right
+    if r > 0 && c < COLUMNS - 1 && predicate(state[r - 1][c + 1]) {
+        count += 1;
+    }
+
+    // down
+    if r < ROWS - 1 && predicate(state[r + 1][c]) {
+        count += 1;
+    }
+
+    // down left
+    if r < ROWS - 1 && c > 0 && predicate(state[r + 1][c - 1]) {
+        count += 1;
+    }
+
+    // down right
+    if r < ROWS - 1 && c < COLUMNS - 1 && predicate(state[r + 1][c + 1]) {
+        count += 1;
+    }
+
+    // left
+    if c > 0 && predicate(state[r][c - 1]) {
+        count += 1;
+    }
+
+    // right
+    if c < COLUMNS - 1 && predicate(state[r][c + 1]) {
+        count += 1;
+    }
+
+    count
+}
+
+// brian's brain
+// TODO: convert to trait?
+// fn get_next_cell_state(state: SimState, r: usize, c: usize) -> CellState {
+//     if state[r][c] == CellState::Alive {
+//         return CellState::Dying;
+//     }
+//
+//     if state[r][c] == CellState::Dying {
+//         return CellState::Dead;
+//     }
+//
+//     // get count of surrounding cells of target type
+//     let target_count = get_cell_count(state, r, c, |target| target == CellState::Alive);
+//     if target_count == 2 {
+//         CellState::Alive
+//     } else {
+//         CellState::Dead
+//     }
+// }
+
+// Conway's game of life
+fn get_next_cell_state(state: SimState, r: usize, c: usize) -> CellState {
+    // get count of surrounding cells of target type
+    let target_count = get_cell_count(state, r, c, |target| target == CellState::Alive);
+
+    if target_count < 2 || target_count > 3 {
+        return CellState::Dead;
+    }
+
+    if target_count == 3 {
+        return CellState::Alive;
+    }
+
+    state[r][c]
 }
 
 #[macroquad::main("Life")]
 async fn main() {
-    let w = screen_width() as usize;
-    let h = screen_height() as usize;
+    request_new_screen_size(1024., 1024.);
+    next_frame().await;
 
-    let mut cells = vec![CellState::Dead; w * h];
-    let mut buffer = vec![CellState::Dead; w * h];
+    // create a 2d array of size w * h
+    let mut state: SimState = [[CellState::Dead; COLUMNS]; ROWS];
+    let mut temp: SimState = [[CellState::Dead; COLUMNS]; ROWS];
+
+    assert_eq!(
+        std::mem::size_of_val(&state),
+        std::mem::size_of_val(&temp),
+        "State and temp matrices are not of the same size"
+    );
 
     // init state
-    // for i in 0..cells.len() {
-    //     if rand::gen_range(0, 3) == 0 {
-    //         cells[i] = CellState::Alive;
+    // for r in 0..state.len() {
+    //     for c in 0..state.len() {
+    //         if rand::gen_range(0, 5) == 0 {
+    //             state[r][c] = CellState::Alive;
+    //         }
     //     }
     // }
 
-    // not sure wtf this does
-    let mut image = Image::gen_image_color(w as u16, h as u16, WHITE);
-    let texture = Texture2D::from_image(&image);
+    let desired_cell_size: f32 = screen_width() / COLUMNS as f32;
 
     // main simulation loop
     loop {
+        // clear_background(WHITE);
+
         // break
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
 
-        // update cell state
-        for i in 0..cells.len() {
-            // rule 30, alive if out of the cell and its neighbors, exactly 1 was alive
-
-            let mut alive_count = 0;
-
-            // check left
-            if i > 0 && cells[i - 1] == CellState::Alive {
-                alive_count += 1;
-            }
-
-            // curr
-            if cells[i] == CellState::Alive {
-                alive_count += 1;
-            }
-
-            // right
-            if i < cells.len() - 1 && cells[i] == CellState::Alive {
-                alive_count += 1;
-            }
-
-            // update cell state
-            if alive_count == 1 {
-                buffer[i] = CellState::Alive;
-            } else if alive_count > 1 {
-                buffer[i] = CellState::Dying;
-            } else {
-                buffer[i] = CellState::Dead;
-            }
-        }
-
         // spawn alive cells on mouse click
         if is_mouse_button_down(MouseButton::Left) {
             let (x, y) = mouse_position();
-            let column = x as usize;
-            let row = y as usize;
-            let idx = row * w + column;
-            buffer[idx] = CellState::Alive;
-            println!("row: {}, col: {}, idx: {}", row, column, idx);
+            let column = (x / desired_cell_size) as usize;
+            let row = (y / desired_cell_size) as usize;
+
+            // bounds check
+            if (row > 0 && row < ROWS - 1) && (column > 0 && column < COLUMNS - 1) {
+                state[row][column] = CellState::Alive;
+
+                state[row - 1][column] = CellState::Alive;
+                state[row + 1][column] = CellState::Alive;
+
+                state[row][column - 1] = CellState::Alive;
+                state[row][column + 1] = CellState::Alive;
+            }
+        }
+
+        // update cell state
+        for r in 0..state.len() {
+            for c in 0..state[r].len() {
+                temp[r][c] = get_next_cell_state(state, r, c);
+            }
         }
 
         // render cell state
-        for i in 0..cells.len() {
-            cells[i] = buffer[i];
-            image.set_pixel(
-                (i % w) as u32,
-                (i / w) as u32,
-                match cells[i] {
-                    CellState::Alive => BLACK,
-                    CellState::Dying => ORANGE,
-                    CellState::Dead => WHITE,
-                },
-            );
+        for r in 0..temp.len() {
+            for c in 0..temp[r].len() {
+                let cell = temp[r][c];
+
+                // update state
+                state[r][c] = cell;
+
+                // bg size - 1 px to create a nice juicy border
+                let curr_cell_size = desired_cell_size - 1.;
+
+                draw_rectangle(
+                    c as f32 * desired_cell_size,
+                    r as f32 * desired_cell_size,
+                    desired_cell_size,
+                    desired_cell_size,
+                    BLACK,
+                );
+
+                draw_rectangle(
+                    c as f32 * desired_cell_size + 1.,
+                    r as f32 * desired_cell_size + 1.,
+                    curr_cell_size,
+                    curr_cell_size,
+                    cell.color(),
+                );
+            }
         }
-
-        texture.update(&image);
-
-        draw_texture(&texture, 0., 0., WHITE);
 
         next_frame().await
     }
